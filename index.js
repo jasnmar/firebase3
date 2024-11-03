2d13087502527f1fce998e79168987bd63acdefc
jasnmar <jasnmar@gmail.com>
2024-11-03 10:41:00 -0500

    starting anew

---
+++ index.js
@@ -0,0 +1,143 @@
+
+import { initializeApp } from "firebase/app";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword,
+  sendEmailVerification, 
+  signInWithEmailAndPassword,
+  signOut 
+} from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJwrc6CKnA4HqFrWbG5eIf4186JgeMg7w",
+  authDomain: "moody-3cb76.firebaseapp.com",
+  projectId: "moody-3cb76",
+  storageBucket: "moody-3cb76.firebasestorage.app",
+};
+
+const app = initializeApp(firebaseConfig);
+console.log('app: ', app)
+const auth = getAuth(app);
+console.log('auth: ', auth)
+/* === UI === */
+
+/* == UI - Elements == */
+
+const viewLoggedOut = document.getElementById("logged-out-view")
+const viewLoggedIn = document.getElementById("logged-in-view")
+
+const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
+
+const emailInputEl = document.getElementById("email-input")
+const passwordInputEl = document.getElementById("password-input")
+
+const signInButtonEl = document.getElementById("sign-in-btn")
+const createAccountButtonEl = document.getElementById("create-account-btn")
+
+const signOutButtonEl = document.getElementById("sign-out-btn")
+
+/* == UI - Event Listeners == */
+
+signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
+
+signInButtonEl.addEventListener("click", authSignInWithEmail)
+createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+
+signOutButtonEl.addEventListener("click", authSignOut)
+
+/* === Main Code === */
+
+showLoggedOutView()
+
+/* === Functions === */
+
+/* = Functions - Firebase - Authentication = */
+
+function authSignInWithGoogle() {
+    console.log("Sign in with Google")
+}
+
+async function authSignInWithEmail() {
+  console.log("Sign in with email and password")
+  const email = emailInputEl.value
+  const password = passwordInputEl.value
+  try{
+    const userCredential = await signInWithEmailAndPassword(auth, email, password)
+    const user = userCredential.user
+    if(user) {
+      clearAuthFields()
+      showLoggedInView()
+    }
+  } catch (error) {
+    const errorCode = error.code
+    const errorMessage = error.message
+    console.error(errorCode, errorMessage)
+  }
+
+}
+
+async function authCreateAccountWithEmail() {
+  console.log("Sign up with email and password")
+  const email = emailInputEl.value
+  const password = passwordInputEl.value
+  try {
+    const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+    const user = userCredential.user
+    try {
+      const emailSent = await sendEmailVerification(auth.currentUser)
+      console.log('emailSent: ')
+    } catch (error) {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.error(errorCode, errorMessage)
+    }
+    if(user) {
+      showLoggedInView()
+    }
+  } catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.error(errorCode, errorMessage)
+  }
+}
+
+async function authSignOut() {
+      try {
+        const signOutVar = await signOut(auth)
+        showLoggedOutView()
+        clearAuthFields()
+      } catch (error) {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.error(errorCode, errorMessage)
+      }
+}
+
+/* == Functions - UI Functions == */
+
+function showLoggedOutView() {
+  hideView(viewLoggedIn)
+  showView(viewLoggedOut)
+}
+
+function showLoggedInView() {
+  hideView(viewLoggedOut)
+  showView(viewLoggedIn)
+}
+
+function showView(view) {
+  view.style.display = "flex"
+}
+
+function hideView(view) {
+  view.style.display = "none"
+}
+
+function clearInputField(field) {
+  field.value = ''
+}
+
+function clearAuthFields() {
+  clearInputField(emailInputEl)
+  clearInputField(passwordInputEl)
+}
